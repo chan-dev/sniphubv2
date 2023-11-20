3ad6ed1985eca6a0bdbedd775574b81bdda74ba1
chan-dev <christian.m.austria@gmail.com>
2023-11-20 15:22:51 +0800

    Add basic setup

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,7 @@
+export const environment = {
+  firebaseConfig: {
+    apiKey: "AIzaSyAARNdyn9ILIr0NP99qXco-WiFQ3w6hEGM",
+    authDomain: "sniphubv2.firebaseapp.com",
+    projectId: "sniphubv2",
+  }
+};
