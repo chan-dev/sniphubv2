1e668329da8479a4269f388dfbc6ef148b4272f3
chan-dev <christian.m.austria@gmail.com>
2023-12-09 12:36:52 +0800

    Add context menu

--- src/app/components/list/list.component.ts
+++ src/app/components/list/list.component.ts
@@ -1,18 +1,30 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, ViewChild, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterLink } from '@angular/router';
+import { NgIconComponent } from '@ng-icons/core';
+import { MatMenu, MatMenuModule, MatMenuTrigger } from '@angular/material/menu';
 
 import { CollapsibleList } from '../../models/list';
-import { NgIconComponent } from '@ng-icons/core';
 
 @Component({
   selector: 'app-list',
   standalone: true,
-  imports: [CommonModule, NgIconComponent, RouterLink],
+  imports: [CommonModule, MatMenuModule, NgIconComponent, RouterLink],
   templateUrl: './list.component.html',
   styles: ``,
 })
 export class ListComponent {
   @Input() list!: CollapsibleList;
   @Input() activeSnippetId?: string;
+
+  @ViewChild(MatMenuTrigger) menuTrigger!: MatMenuTrigger;
+
+  openMenu(event: MouseEvent) {
+    event.preventDefault();
+    this.menuTrigger.toggleMenu();
+  }
+
+  disableContextMenu() {
+    this.menuTrigger.closeMenu();
+  }
 }
