b2012f7d75f011b4d30ec0a123422daaec644dc5
chan-dev <christian.m.austria@gmail.com>
2023-12-13 11:29:10 +0800

    Optimize performance for click outside directive

--- src/app/directives/click-outside.directive.ts
+++ src/app/directives/click-outside.directive.ts
@@ -1,30 +1,48 @@
 import {
+  AfterViewInit,
   Directive,
   ElementRef,
   EventEmitter,
+  NgZone,
   Output,
+  Renderer2,
   inject,
 } from '@angular/core';
 
 @Directive({
   selector: '[appClickOutside]',
   standalone: true,
-  host: {
-    '(document:click)': 'handleClick($event)',
-  },
 })
-export class ClickOutsideDirective {
+export class ClickOutsideDirective implements AfterViewInit {
   @Output() appClickOutside = new EventEmitter();
 
   private elementRef = inject(ElementRef);
+  private renderer = inject(Renderer2);
+  private zone = inject(NgZone);
+
+  ngAfterViewInit(): void {
+    this.zone.runOutsideAngular(() => {
+      this.addClickHandler();
+    });
+  }
+
+  private addClickHandler() {
+    // This prevents running change detection on every click since
+    // the directive is attached to the document
+    this.renderer.listen(document, 'click', (event: MouseEvent) => {
+      this.handleClick(event);
+    });
+  }
 
   handleClick(event: MouseEvent) {
     if (!this.elementRef.nativeElement.contains(event.target)) {
-      this.appClickOutside.emit(event);
+      this.zone.run(() => {
+        this.appClickOutside.emit(event);
+      });
     }
-
-    // Use this instead of stopPropagation so it also stops
-    // propagation the event handler on the same element (in this case `document`)
-    event.stopImmediatePropagation();
+    event.stopPropagation();
+    // Comment this one as it stops other elements which use this directive
+    // from receiving the event
+    // event.stopImmediatePropagation();
   }
 }
