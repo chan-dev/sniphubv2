1f1c7b3906bc432562e6defe4d01a3336b21f819
chan-dev <christian.m.austria@gmail.com>
2024-01-20 14:16:00 +0800

    Disable editor keybinding conflicting with search shortcut
    Fixed issue #2

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -12,6 +12,19 @@ import { provideAnimations } from '@angular/platform-browser/animations';
 const monacoConfig: NgxMonacoEditorConfig = {
   // baseUrl: './assets', // configure base path for monaco editor. Starting with version 8.0.0 it defaults to './assets'. Previous releases default to '/assets'
   defaultOptions: { scrollBeyondLastLine: false }, // pass default options to be used
+  onMonacoLoad: () => {
+    // NOTE:
+    // A workaround to disable default CTRL/CMD + K keybinding which is used
+    // as shortcut key for searching code snippets.
+    //
+    // https://github.com/microsoft/monaco-editor/issues/102#issuecomment-1300619657
+    window.monaco.editor.addKeybindingRules([
+      {
+        keybinding: window.monaco.KeyMod.CtrlCmd + window.monaco.KeyCode.KeyK,
+        command: null,
+      },
+    ]);
+  },
 };
 
 export const appConfig: ApplicationConfig = {
